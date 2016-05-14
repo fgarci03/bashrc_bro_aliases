7578a2b6e669835175c49dd320212787ccdb049a
Filipe Garcia <garcia.filipeandre@gmail.com>
2016-05-14 03:52:34 +0100

    added aliases

---
+++ bash_bro_aliases.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# the regular ones
+alias 'plz'='sudo'
+alias 'fucking'='sudo apt-get'
+alias 'fuck'='sudo "$BASH" -c "$(history -p !!)"'
+alias 'fuckoff'='fucking autoremove --purge'
+alias 'godlike'='sudo su'
+
+# utilities
+alias 'chrome'='google-chrome-stable'
+alias 'pewpew'='kill -9'
+alias 'dog'='cat'
+alias 'dude'='man'
+alias 'wtf'='git blame'
+
+# because fuck you, that's why
+alias 'nano'='vim'
+alias 'emacs'='vim'
+
+# close
+alias 'gtfo'='sudo shutdown now -P'
+alias 'brb'='sudo reboot now'
+alias 'cya'='exit'
